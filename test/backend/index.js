538204138357d77ef877117be43a15b4672394c1
Thao <nguyenthithuy18012002@gmail.com>
2023-11-01 02:32:04 +0700

    test

---
+++ test/backend/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+const db= mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'test',
+})
+
+
+app.get("/", (req, res)=>{
+    res.json("Thao ne!")
+})
+
+app.get("/books", (req, res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+    db.query(q,[values], (err, data)=>{
+        if(err) return res.json(err)
+        return res.json("Successfully!")
+    })
+})
+
+app.listen(8800, () =>{
+    console.log('Connected to backend!')
+});
