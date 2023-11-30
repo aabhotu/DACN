5985b01ae4157d72536d17280b1d9594a2f07303
Thao <nguyenthithuy18012002@gmail.com>
2023-11-30 22:52:03 +0700

    hour

--- envirMo/frontend/src/components/chart/Chart.jsx
+++ envirMo/frontend/src/components/chart/Chart.jsx
@@ -31,8 +31,8 @@ const Chart = () => {
     useEffect(() => {
         const fetchData = async () =>{
             try{
-                const datas = await axios.get("http://localhost:8800/specifi")
-                console.log(datas.data)
+                const datas = await axios.get("http://localhost:8800/tbl_tracker")
+                //console.log(datas.data)
                 setParams(datas.data)
             }
             catch(err){
@@ -44,51 +44,59 @@ const Chart = () => {
     
     const tems = []
     const hums = []
-    const pres = []
+    const luxs = []
     const dusts = []
     let weekday
     params.map(param => {
-        tems.push(param.tem)
-        hums.push(param.hudmi)
-        pres.push(param.press)
-        dusts.push(param.dust)
+        tems.push(param.temp)
+        hums.push(param.hud)
+        luxs.push(param.lux)
+        // dusts.push(param.dust)
     })
-    console.log(dusts)
+    console.log(luxs)
 
     return (
         <div className='chart'>
             <Line
                 data={{
                 labels: params.map(param => {
-                    const dateData = new Date(param.time)
-                    const date = dateData.getDay();
                     
-                    switch (date) {
-                        case 0:
-                        weekday = 'Sunday';
-                        break;
-                        case 1:
-                        weekday = 'Monday';
-                        break;
-                        case 2:
-                        weekday = 'Tuesday';
-                        break;
-                        case 3:
-                        weekday = 'Wednesday';
-                        break;
-                        case 4:
-                        weekday = 'Thursday';
-                        break;
-                        case 5:
-                        weekday = 'Friday';
-                        break;
-                        case 6:
-                        weekday = 'Saturday';
-                        break;
-                        default:
-                        weekday = 'Undefined';
-                    }
-                    return weekday;
+                    const dateData = new Date(param.tim)
+
+                    const hour = dateData.getHours()
+                    //console.log(dateData)
+
+                    //if (hour %2 ===0) return hour 
+                    return hour
+                    // const date = dateData.getDay();
+
+                    
+                    // switch (date) {
+                    //     case 0:
+                    //     weekday = 'Sunday';
+                    //     break;
+                    //     case 1:
+                    //     weekday = 'Monday';
+                    //     break;
+                    //     case 2:
+                    //     weekday = 'Tuesday';
+                    //     break;
+                    //     case 3:
+                    //     weekday = 'Wednesday';
+                    //     break;
+                    //     case 4:
+                    //     weekday = 'Thursday';
+                    //     break;
+                    //     case 5:
+                    //     weekday = 'Friday';
+                    //     break;
+                    //     case 6:
+                    //     weekday = 'Saturday';
+                    //     break;
+                    //     default:
+                    //     weekday = 'Undefined';
+                    // }
+                    // return weekday;
 
                 }),
                 datasets: [
@@ -105,15 +113,9 @@ const Chart = () => {
                     fill: false
                     },
                     {
-                    data: pres,
+                    data: luxs,
                     label: "Áp suất",
                     borderColor: "#3cba9f",
-                    fill: false
-                    },
-                    {
-                    data: dusts,
-                    label: "Nồng độ bụi mịn",
-                    borderColor: "#e8c3b9",
                     fill: false
                     }
                 ]
